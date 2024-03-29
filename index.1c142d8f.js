e2dba13ac2aa0e7e7b96658ad1c8ad687517d1e1
Gorbunov Kirill <1kvgxiaomi1711@gmail.com>
2024-03-29 20:19:49 +0200

    Updates

---
+++ index.1c142d8f.js
@@ -0,0 +1,2 @@
+!function(){let t=document.querySelector(".football__box"),e=document.querySelector(".football__ball");t.addEventListener("click",l=>{e.style.left=`${l.clientX-Math.round(t.getBoundingClientRect().left)}px`,e.style.top=`${l.clientY-Math.round(t.getBoundingClientRect().top)}px`})}();
+//# sourceMappingURL=index.1c142d8f.js.map
---
+++ index.7b2e8ba4.js
@@ -0,0 +1,2 @@
+const t=document.querySelector(".football__box"),e=document.querySelector(".football__ball");t.addEventListener("click",l=>{e.style.left=`${l.clientX-Math.round(t.getBoundingClientRect().left)}px`,e.style.top=`${l.clientY-Math.round(t.getBoundingClientRect().top)}px`});
+//# sourceMappingURL=index.7b2e8ba4.js.map
